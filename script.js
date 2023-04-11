130c45969d2df84c53c507246963104502fac137
billmukenga01 <billmukenga01@gmail.com>
2023-04-11 18:50:11 +0300

    created a grid box with 16x16

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 const container = document.querySelector('.container');
+const body = document.querySelector('body');
 let gridSize = 16;
 
-for(i=1; i<=gridSize; i++){
-    const gridElement = document.createElement('div');
-    gridElement.setAttribute('class', 'grid-element');
-    container.appendChild(gridElement);
+
+body.style.display = 'flex';
+body.style.justifyContent = 'center';
+body.style.alignItems = 'center';
+container.style.maxHeight = '320px';
+container.style.maxWidth = '320px'
+
+for (let i = 1; i <= gridSize*gridSize; ++i) {
+    const newDiv = document.createElement('div');
+    newDiv.setAttribute('class', 'grid-items');
+    newDiv.style.backgroundColor = 'blue';
+    container.appendChild(newDiv);
 }
+  
+
+container.style.display = 'grid';
+container.style.gridTemplateRows = `repeat(${gridSize}, 20px)`;
+container.style.gridTemplateColumns = `repeat(${gridSize}, 20px)`;
+
