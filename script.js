2debcce4805deddf3961d651903bcc44c121b025
billmukenga01 <billmukenga01@gmail.com>
2023-04-13 16:15:54 +0300

    feat: implemented  hover effect on grid-container

--- script.js
+++ script.js
@@ -1,23 +1,51 @@
 const container = document.querySelector('.container');
+const head = document.querySelector('head');
 const body = document.querySelector('body');
-let gridSize = 16;
+const style = document.createElement('style');
+const selectorDiv = document.createElement('div');
+let gridSize = 40;
 
+selectorDiv.setAttribute('class', 'selector-div');
 
-body.style.display = 'flex';
-body.style.justifyContent = 'center';
-body.style.alignItems = 'center';
-container.style.maxHeight = '320px';
-container.style.maxWidth = '320px'
+style.textContent = `
+    body{
+        display:flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    .grid-items{
+        border: 0.001rem solid grey;
+    }
+    .clear{
+        background-color:white;
+    }.grey-color{
+        background-color:grey;
+    }
+    .selector-div{
+        background-color:blue;
+        margin-bottom: 2rem;
+        width:50vw;
+        height:20vh;
+    }
+    .container{
+        display:grid; 
+        border: 0.001rem solid lightgrey;
+        height:50vh; 
+        width:50vw; 
+        grid-template-columns:repeat(${gridSize}, 1fr)
+    }
+`;
+body.insertBefore(selectorDiv, container);
+head.appendChild(style);
 
 for (let i = 1; i <= gridSize*gridSize; ++i) {
     const newDiv = document.createElement('div');
     newDiv.setAttribute('class', 'grid-items');
-    newDiv.style.backgroundColor = 'blue';
     container.appendChild(newDiv);
 }
-  
 
-container.style.display = 'grid';
-container.style.gridTemplateRows = `repeat(${gridSize}, 20px)`;
-container.style.gridTemplateColumns = `repeat(${gridSize}, 20px)`;
-
+container.addEventListener('mouseover', e =>{
+    if(e.target.classList.contains('grid-items')){
+        e.target.classList.add('grey-color');
+    }
+})
