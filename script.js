becbe5525ce3bd75c03263c79e71e692a4279019
billmukenga01 <billmukenga01@gmail.com>
2023-04-21 15:14:11 +0300

    feat: implemented the layout of the page

--- script.js
+++ script.js
@@ -2,19 +2,42 @@ const container = document.querySelector('.container');
 const head = document.querySelector('head');
 const body = document.querySelector('body');
 const style = document.createElement('style');
+const containerOne = document.createElement('div');
 const selectorDiv = document.createElement('div');
 let gridSize = 40;
 
 selectorDiv.setAttribute('class', 'selector-div');
+containerOne.setAttribute('class', 'container__one')
 
 style.textContent = `
+    *{
+        margin:0;
+        padding:0;
+        box-sizing:border-box;
+    }
+    
     body{
         display:flex;
         flex-direction: column;
-        align-items: center;
     }
-    .grid-items{
-        border: 0.001rem solid grey;
+    
+    header, footer{
+        background-color:blue;
+        height: 15vh;
+        
+    }
+    header{
+        margin-bottom: 1.5em
+    }
+    
+    .container{
+        display:flex;
+        flex-wrap: wrap;
+        gap: 2rem;
+        height: 70vh;
+        margin-bottom: 1.5em;
+        justify-content:center;
+        align-content: center;
     }
     .clear{
         background-color:white;
@@ -23,25 +46,25 @@ style.textContent = `
     }
     .selector-div{
         background-color:blue;
-        margin-bottom: 2rem;
-        width:50vw;
-        height:20vh;
+        width:200px;
+        height:500px;
     }
-    .container{
+    .container__one{
         display:grid; 
         border: 0.001rem solid lightgrey;
-        height:50vh; 
-        width:50vw; 
+        height:500px; 
+        width:500px; 
         grid-template-columns:repeat(${gridSize}, 1fr)
     }
 `;
-body.insertBefore(selectorDiv, container);
+container.appendChild(containerOne);
+container.insertBefore(selectorDiv, containerOne);
 head.appendChild(style);
 
 for (let i = 1; i <= gridSize*gridSize; ++i) {
     const newDiv = document.createElement('div');
     newDiv.setAttribute('class', 'grid-items');
-    container.appendChild(newDiv);
+    containerOne.appendChild(newDiv);
 }
 
 container.addEventListener('mouseover', e =>{
