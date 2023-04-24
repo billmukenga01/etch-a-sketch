515bf5051344d7dd2faa60a7a75546b90778ac8e
billmukenga01 <billmukenga01@gmail.com>
2023-04-24 18:11:09 +0300

    style: simpe box shadow and buttons

--- script.js
+++ script.js
@@ -4,10 +4,20 @@ const body = document.querySelector('body');
 const style = document.createElement('style');
 const containerOne = document.createElement('div');
 const selectorDiv = document.createElement('div');
+const reset = document.createElement('button');
+const clear = document.createElement('button');
+const header = document.querySelector('header');
+const heading = document.querySelector('p');
 let gridSize = 40;
 
 selectorDiv.setAttribute('class', 'selector-div');
 containerOne.setAttribute('class', 'container__one')
+reset.setAttribute('class', 'button')
+reset.textContent = "Reset"
+//
+clear.setAttribute('class', 'button')
+clear.textContent = "Clear"
+
 
 style.textContent = `
     *{
@@ -17,15 +27,24 @@ style.textContent = `
     }
     
     header, footer{
-        background-color:blue;
-        height: 15vh;
+        background-color:lightgrey;
+        height: 10vh;
     }
+    header{
+       text-align:center;
+       font-size: 5em
+    }
+    .button{
+        width:7rem;
+        height:3rem;
+        font: "times-new-roman"
 
+    }
     .container{
         display:flex;
         flex-wrap: wrap;
         gap: 2rem;
-        height: 70vh;
+        height: 80vh;
         justify-content:center;
         align-content: center;
         
@@ -40,7 +59,13 @@ style.textContent = `
     }
 
     .selector-div{
-        background-color:blue;
+        display:flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content:center;
+        gap: 1rem;
+        border-radius:5px;
+        box-shadow:0 3px 10px rgb(211, 211, 211);
         width:200px;
         height:80%;
     }
@@ -49,13 +74,14 @@ style.textContent = `
         display:grid; 
         border: 0.001rem solid lightgrey;
         height:80%; 
-        width:500px; 
+        width:700px; 
         box-shadow: 0 3px 10px rgb(211, 211, 211);
         grid-template-columns:repeat(${gridSize}, 1fr)
     }
 `;
 container.appendChild(containerOne);
 container.insertBefore(selectorDiv, containerOne);
+header.textContent = "Etch-A-Sketch"
 head.appendChild(style);
 
 for (let i = 1; i <= gridSize*gridSize; ++i) {
@@ -69,3 +95,6 @@ container.addEventListener('mouseover', e =>{
         e.target.classList.add('grey-color');
     }
 })
+
+selectorDiv.appendChild(clear);
+selectorDiv.appendChild(reset);
